712e289102147690feb5bbad5d1c5f23addb04d2
Feyzanur-Ak <senturkfeyzanur96@gmail.com>
2025-01-21 11:34:49 +0300

    getTasks metodu oluşturuldu

--- src/main/java/org/example/entity/TaskData.java
+++ src/main/java/org/example/entity/TaskData.java
@@ -36,9 +36,37 @@ public class TaskData {
         return unassignedTasks;
     }
 
-    public String getTasks(String assignee){
-
+    public Set<Task> getTasks(String assignee){
+     switch (assignee.toLowerCase()){
+         case "ann" :
+             return  annsTasks;
+         case "bob" :
+             return  bobsTasks;
+         case "carol" :
+             return  carolsTasks;
+         case "all" :
+             return  getUnion(Set.of(annsTasks,bobsTasks,carolsTasks));
+     }
     }
 
+   public  Set<Task> getUnion(Set<Set<Task>> sets){
+       //Burada yeni bir Set içerisinde Set<Task> Yapısı eklenecektir.
+       Set<Task> union = new HashSet<>();
+       for(Set<Task> set : sets) {
+           union.addAll(set);
+       }
+       return union;
+   }
 
+   public  Set<Task> getIntersect(Set<Task> set1,Set<Task> set2) {
+       Set<Task> intersection = new HashSet<>(set1);
+       intersection.retainAll(set2);
+       return intersection;
+   }
+
+    public  Set<Task> getDifference(Set<Task> set1,Set<Task> set2) {
+        Set<Task> difference = new HashSet<>(set1);
+        difference.removeAll(set2);
+        return difference;
+    }
 }
