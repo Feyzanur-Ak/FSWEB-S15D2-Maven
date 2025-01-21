e98b0428814f820ff0cb5560ad5d3a149bab3c09
Feyzanur-Ak <senturkfeyzanur96@gmail.com>
2025-01-21 11:25:42 +0300

    Entity package ve içerisindeki Class enum yapıalrı oluşturuldu

---
+++ src/main/java/org/example/entity/TaskData.java
@@ -0,0 +1,44 @@
+package org.example.entity;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class TaskData {
+
+
+
+    Set<Task> annsTasks=new HashSet<>();
+    Set<Task> bobsTasks=new HashSet<>();
+    Set<Task> carolsTasks=new HashSet<>();
+    Set<Task> unassignedTasks =new HashSet<>();
+
+
+    public TaskData(Set<Task> annsTasks, Set<Task> bobsTasks, Set<Task> carolsTasks, Set<Task> unassignedTasks) {
+        this.annsTasks = annsTasks;
+        this.bobsTasks = bobsTasks;
+        this.carolsTasks = carolsTasks;
+        this.unassignedTasks = unassignedTasks;
+    }
+
+    public Set<Task> getAnnsTasks() {
+        return annsTasks;
+    }
+
+    public Set<Task> getBobsTasks() {
+        return bobsTasks;
+    }
+
+    public Set<Task> getCarolsTasks() {
+        return carolsTasks;
+    }
+
+    public Set<Task> getUnassignedTasks() {
+        return unassignedTasks;
+    }
+
+    public String getTasks(String assignee){
+
+    }
+
+
+}
